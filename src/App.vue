93d0ed8231d424c7f99c666b6f3379f20ff8d613
Todd0554 <toddy9229@outlook.com>
2022-12-18 11:00:17 +1100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Im h1</h1>
+  <Index></Index>
+  <HelloWorld :msg="'heyhey'"></HelloWorld>
+</template>
+
+<script setup lang="ts">
+import Index from "@/views/Index.vue"
+import HelloWorld from "@/components/HelloWorld.vue"
+</script>
+
+
+
