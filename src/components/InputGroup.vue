2ce8ed044adcf1de76d555fda6a861a7a3e81141
Todd0554 <toddy9229@outlook.com>
2022-12-21 19:30:08 +1100

    counter for sms code and phone number regex

--- src/components/InputGroup.vue
+++ src/components/InputGroup.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="text-group">
         <div class="input-group" :class="{'is-invalid': error}">
-            <input :type="type" :value="value" :placeholder="placeholder" />
-            <button v-if="btnTitle" :disabled="disabled">{{btnTitle}}</button>
+            <input @input="$emit('update:modelValue', ($event?.target as HTMLInputElement).value)" :type="type" :value="modelValue" :placeholder="placeholder" />
+            <button @click="$emit('btnClick')" v-if="btnTitle" :disabled="disabled">{{btnTitle}}</button>
         </div>
         <div v-if="error" class="invalid-feedback">{{error}}</div>
     </div>
@@ -12,18 +12,23 @@
 import {  } from 'vue'
 interface PropsType{
     type:string,
-    value:string,
+    modelValue:string,
     placeholder:string,
     btnTitle?:string,
     disabled?:boolean,
     error:string
 }
+// withDefaults 用来设置默认值， 配合defineProps使用
 withDefaults(defineProps<PropsType>(),{
     type:"",
-    value:"",
+    modelValue:"",
     placeholder:"",
     btnTitle:"",
     disabled:false,
     error:""
-})
+});
+defineEmits([
+    "btnClick",
+    "update:modelValue"
+])
 </script>
--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -3,15 +3,16 @@
         <div class="logo">
             <img src="@/assets/logo.png" alt="my log in image">
         </div>
-        <InputGroupVue 
+        <InputGroup 
         type="number" 
         v-model="phone" 
         placeholder="phone number" 
         :btn-title="btnTitle"
         :disabled="disabled"
         :error="phoneError"
+        @btnClick="getVerifyCode"
         />
-        <InputGroupVue 
+        <InputGroup 
         type="number" 
         v-model="verifyCode" 
         placeholder="SMS Code" 
@@ -29,8 +30,9 @@
 
 <script lang='ts' setup>
 import { ref } from 'vue'
-import InputGroupVue from '@/components/InputGroup.vue';
+import InputGroup from '@/components/InputGroup.vue';
 
+// react attribute
 const phone = ref("")
 const btnTitle = ref("Get SMS Code")
 const disabled = ref(false)
@@ -38,4 +40,40 @@ const phoneError = ref("")
 
 const verifyCode = ref("")
 const codeError = ref("")
+
+// method
+const validatePhoneNumber = ()=>{
+    if (!phone.value){
+        phoneError.value = "phone number can't be empty!"
+        return false
+    } else if (!/^(?:\+?(61))? ?(?:\((?=.*\)))?(0?[2-57-8])\)? ?(\d\d(?:[- ](?=\d{3})|(?!\d\d[- ]?\d[- ]))\d\d[- ]?\d[- ]?\d{3})$/.test(phone.value)){
+        phoneError.value = "this number is not validated!"
+        return false
+    } else {
+        phoneError.value = ""
+        return true
+    }
+}
+const handleCounter = ()=>{
+    let time = 60
+    let timer = setInterval(()=>{
+        if (time == 0){
+            clearInterval(timer);
+            btnTitle.value = "Get SMS Code"
+            disabled.value = false
+        } else {
+            btnTitle.value = "Retry after " + time + "s";
+            disabled.value = true
+            time--
+        }
+    }, 1000)
+}
+const getVerifyCode = ()=>{
+    // validate phone number
+    if (validatePhoneNumber()){
+        // setTime
+        handleCounter()
+    }
+}
+
 </script>
